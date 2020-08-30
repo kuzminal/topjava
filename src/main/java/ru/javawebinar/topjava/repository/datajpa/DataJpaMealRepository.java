e355df71c03a8e3cdb87216292053e2361b04052
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2020-08-30 13:58:23 +0300

    Пока убрал профили и сделал свой отдельный конфиг для тестов приложения Профили работали некорректно Реализовал частично репозиторий еды

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,7 +1,11 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -9,16 +13,19 @@ import java.util.List;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
-
+    private static final Sort SORT_DATE_TIME = Sort.by(Sort.Direction.ASC, "dateTime");
     private final CrudMealRepository crudRepository;
+    private CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository crudUserRepository) {
         this.crudRepository = crudRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        meal.setUser(crudUserRepository.getOne(userId));
+        return crudRepository.save(meal);
     }
 
     @Override
@@ -28,12 +35,12 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Override
     public Meal getById(int id, int userId) {
-        return null;
+        return crudRepository.findById(id).filter(meal -> meal.getUser().getId() == userId).orElse(null);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAll(SORT_DATE_TIME);
     }
 
     @Override
